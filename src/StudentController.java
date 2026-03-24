3b35464a33004f2aac484af3098adaad51965678
anngiepp <angie.portocarrero@tecsup.edu.pe>
2026-03-24 15:27:04 -0500

    Update StudentController

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,4 +1,9 @@
 public class StudentController {
+    
+    public String getNombre() {
+        return "Ana";
+    }
+    
     public static void main(String[] args) {
         System.out.println("Student Controller");
     }
