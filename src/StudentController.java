321b3b85119c573f155b8e4e343ea7f5c3bdad3f
anngiepp <angie.portocarrero@tecsup.edu.pe>
2026-03-24 23:04:27 -0500

    Update StudentController en sprint-1

--- src/StudentController.java
+++ src/StudentController.java
@@ -8,6 +8,10 @@ public class StudentController {
         return "Portocarrero";
     }
     
+    public int getEdad() {
+        return 20;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Student Controller");
     }
